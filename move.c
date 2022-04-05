eae78c741f1b7e182d6474e848316a35c0390792
Michail Chatzipanagiotou <mchatzip@c2s16d91.42wolfsburg.de>
2022-04-05 18:33:52 +0200

    semi rays

--- move.c
+++ move.c
@@ -6,40 +6,53 @@
 /*   By: mchatzip <mchatzip@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 20:12:33 by ekraujin          #+#    #+#             */
-/*   Updated: 2022/04/05 15:11:59 by mchatzip         ###   ########.fr       */
+/*   Updated: 2022/04/05 18:27:26 by mchatzip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	move3(t_data *game, int keycode)
+{
+	if (keycode == RIGHT)
+	{
+		game->ppos_x += game->dirY * 15 + 0.0001;
+		game->ppos_y -= game->dirX * 15 + 0.0001;
+		draw_character(game, 255);
+		face_direction(game);
+	}
+	if (keycode == LEFT)
+	{
+		game->ppos_x -= game->dirY * 15 + 0.0001;
+		game->ppos_y += game->dirX * 15 + 0.0001;
+		draw_character(game, 255);
+		face_direction(game);
+	}
+}
 
 void	move2(t_data *game, int keycode)
 {
 	if (keycode == STRAFE_L)
 	{
 		game->oldDirX = game->dirX;
-		game->dirX = game->dirX * cos((float)-0.2) - game->dirY * sin((float)-0.2);
-		game->dirY = game->oldDirX * sin((float)-0.2) + game->dirY * cos((float)-0.2);
+		game->dirX = game->dirX * cos((float)-0.2)
+			- game->dirY * sin((float)-0.2);
+		game->dirY = game->oldDirX * sin((float)-0.2)
+			+ game->dirY * cos((float)-0.2);
+		draw_character(game, 255);
+		face_direction(game);
 	}
 	if (keycode == STRAFE_R)
 	{
 		game->oldDirX = game->dirX;
-		game->dirX = game->dirX * cos((float)0.2) - game->dirY * sin((float)0.2);
-		game->dirY = game->oldDirX * sin((float)0.2) + game->dirY * cos((float)0.2);
-	}
-	if (keycode == RIGHT)
-	{
-		game->ppos_x -= game->dirY * 15;
-		game->ppos_y += game->dirX * 15;
+		game->dirX = game->dirX * cos((float)0.2)
+			- game->dirY * sin((float)0.2);
+		game->dirY = game->oldDirX * sin((float)0.2)
+			+ game->dirY * cos((float)0.2);
 		draw_character(game, 255);
 		face_direction(game);
 	}
-	if (keycode == LEFT)
-	{
-		game->ppos_x += game->dirY * 15;
-		game->ppos_y -= game->dirX * 15;
-		draw_character(game, 255);
-		face_direction(game);
-	}
+	move3(game, keycode);
 }
 
 void	move(t_data *game, int keycode)
--- move2.c
+++ move2.c
@@ -6,7 +6,7 @@
 /*   By: mchatzip <mchatzip@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 20:12:33 by ekraujin          #+#    #+#             */
-/*   Updated: 2022/04/05 15:12:32 by mchatzip         ###   ########.fr       */
+/*   Updated: 2022/04/05 18:30:53 by mchatzip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,26 @@
 void	face_direction(t_data *game)
 {
 	int		y;
-	short	i;
+	size_t	i;
 	int		x;
 
-	y = game->ppos_y;
-	x = game->ppos_x;
-	i = 0;
-	while (i++ < 60)
+	i = -1;
+	while (++i || 1)
 	{
-		mlx_pixel_put(game->mlx, game->mlx_win, x + game->dirX * i,
-			y + game->dirY * i, create_trgb(0, 0, 100, 0));
+		if ((!((int)(game->ppos_x + game->dirX * i) % 60)
+			|| !((int)(game->ppos_y + game->dirY * i) % 60)))
+		{
+			x = abs((int)(game->ppos_x + game->dirX * i + 0.0001) / 60);
+			y = abs((int)(game->ppos_y + game->dirY * i + 0.0001) / 60);
+			printf("%lf  %lf\n", game->dirY, game->dirX);
+			if (game->dirX < 0)
+				x++;
+			if (game->dirY < 0)
+				y++;
+			if (game->map[y][x] == '1')
+				break ;
+		}
+		mlx_pixel_put(game->mlx, game->mlx_win, game->ppos_x + game->dirX * i,
+			game->ppos_y + game->dirY * i, create_trgb(0, 0, 60, 0));
 	}
 }
