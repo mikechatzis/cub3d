ce016f73b05f292a8ccb9107a1024a5bebe2b4e4
Michail Chatzipanagiotou <mchatzip@c2s16d91.42wolfsburg.de>
2022-04-06 19:27:22 +0200

    working multi ray-casting with colision

--- move2.c
+++ move2.c
@@ -6,7 +6,7 @@
 /*   By: mchatzip <mchatzip@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 20:12:33 by ekraujin          #+#    #+#             */
-/*   Updated: 2022/04/06 19:06:13 by mchatzip         ###   ########.fr       */
+/*   Updated: 2022/04/06 19:25:21 by mchatzip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,16 +57,10 @@ void	face_direction(t_data *game)
 	i = -1;
 	while (++i || 1)
 	{
-		if ((!((int)(game->ppos_x + game->dirx * i) % 60)
-			|| !((int)(game->ppos_y + game->diry * i) % 60)))
-		{
-			if (!game->check)
-				check(game, i);
-			else
-				check2(game, i);
-			if (game->map[game->y][game->x] == '1')
-				break ;
-		}
+		game->x = abs((int)(game->ppos_x + game->dirx * i) / 60);
+		game->y = abs((int)(game->ppos_y + game->diry * i) / 60);
+		if (game->map[game->y][game->x] == '1')
+			break ;
 		mlx_pixel_put(game->mlx, game->mlx_win, game->ppos_x + game->dirx * i,
 			game->ppos_y + game->diry * i, create_trgb(0, 0, 60, 0));
 	}
@@ -77,18 +71,12 @@ bool	wall_colision_ray(t_data *game)
 	size_t	i;
 
 	i = -1;
-	while (++i < 17)
+	while (++i < 10)
 	{
-		if ((!((int)(game->ppos_x + game->dirx * i) % 60)
-			|| !((int)(game->ppos_y + game->diry * i) % 60)))
-		{
-			if (!game->check)
-				check(game, i);
-			else
-				check2(game, i);
-			if (game->map[game->y][game->x] == '1')
-				return (1);
-		}
+		game->x = abs((int)(game->ppos_x + game->dirx * i) / 60);
+		game->y = abs((int)(game->ppos_y + game->diry * i) / 60);
+		if (game->map[game->y][game->x] == '1')
+			return (1);
 		mlx_pixel_put(game->mlx, game->mlx_win, game->ppos_x + game->dirx * i,
 			game->ppos_y + game->diry * i, create_trgb(0, 60, 0, 0));
 	}
