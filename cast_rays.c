85df36105045fbb6bccef66dfd3c431efdb4f8ed
Edgars Kraujins <ekraujin@c2s9d35.42wolfsburg.de>
2022-04-12 11:38:47 +0200

    improved collision detection

--- cast_rays.c
+++ cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: ekraujin <ekraujin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 20:12:33 by ekraujin          #+#    #+#             */
-/*   Updated: 2022/04/11 23:47:06 by ekraujin         ###   ########.fr       */
+/*   Updated: 2022/04/12 11:37:43 by ekraujin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 bool	wall_colision_up(t_data *game)
 {
-	if (game->map[(int)(game->ppos_y + game->diry * MOVESPEED * 2)]
-		[(int)(game->ppos_x + game->dirx * MOVESPEED * 2)] == '1')
+	if (game->map[(int)(game->ppos_y + game->diry * MOVESPEED)]
+		[(int)(game->ppos_x + game->dirx * MOVESPEED)] == '1')
 		return (1);
 	return (0);
 }
 
 bool	wall_colision_down(t_data *game)
 {
-	if (game->map[(int)(game->ppos_y - game->diry * MOVESPEED * 2)]
-		[(int)(game->ppos_x - game->dirx * MOVESPEED * 2)] == '1')
+	if (game->map[(int)(game->ppos_y - game->diry * MOVESPEED)]
+		[(int)(game->ppos_x - game->dirx * MOVESPEED)] == '1')
 		return (1);
 	return (0);
 }
 
 bool	wall_colision_left(t_data *game)
 {
-	if (game->map[(int)(game->ppos_y - game->planey * MOVESPEED * 2)]
-		[(int)(game->ppos_x - game->planex * MOVESPEED * 2)] == '1')
+	if (game->map[(int)(game->ppos_y - game->planey * MOVESPEED)]
+		[(int)(game->ppos_x - game->planex * MOVESPEED)] == '1')
 		return (1);
 	return (0);
 }
 
 bool	wall_colision_right(t_data *game)
 {
-	if (game->map[(int)(game->ppos_y + game->planey * MOVESPEED * 2)]
-		[(int)(game->ppos_x + game->planex * MOVESPEED * 2)] == '1')
+	if (game->map[(int)(game->ppos_y + game->planey * MOVESPEED)]
+		[(int)(game->ppos_x + game->planex * MOVESPEED)] == '1')
 		return (1);
 	return (0);
 }
