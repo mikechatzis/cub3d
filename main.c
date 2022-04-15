1cb4f60d7c4a39bba7ca8419626339715a2f4f04
Michail Chatzipanagiotou <mchatzip@c2s16d91.42wolfsburg.de>
2022-04-15 14:26:15 +0200

    empty map file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mchatzip <mchatzip@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 20:12:33 by ekraujin          #+#    #+#             */
-/*   Updated: 2022/04/15 14:22:17 by mchatzip         ###   ########.fr       */
+/*   Updated: 2022/04/15 14:26:09 by mchatzip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,9 @@ int	arg_check(t_data *game, int mfd)
 	{
 		temp = skip_empty_lines(game, mfd, temp);
 		if (!temp)
-		{
 			b = 0;
+		if (!temp)
 			break ;
-		}
 		if (!check_validity(temp))
 		{
 			free(temp);
